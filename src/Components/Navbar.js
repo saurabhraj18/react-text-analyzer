4e7cf347660d5091d33a9ede2c1b7ab908b2d4ae
Saurabh Raj <saurabhraj852119@gmail.com>
2026-01-17 12:43:42 +0530

    Fixed Navbar accessibility issue for CI build

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,9 +5,12 @@ export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+
+        {/* Brand */}
+        <span className="navbar-brand">
           {props.title}
-        </a>
+        </span>
+
         <button
           className="navbar-toggler"
           type="button"
@@ -19,20 +22,32 @@ export default function Navbar(props) {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
+              <button
+                type="button"
+                className="nav-link btn btn-link active"
+                aria-current="page"
+              >
                 Home
-              </a>
+              </button>
             </li>
+
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <button
+                type="button"
+                className="nav-link btn btn-link"
+              >
                 About
-              </a>
+              </button>
             </li>
+
           </ul>
-          <form className="d-flex" role="search">
+
+          <form className="d-flex" role="search" onSubmit={(e) => e.preventDefault()}>
             <input
               className="form-control me-2"
               type="search"
@@ -49,5 +64,13 @@ export default function Navbar(props) {
   );
 }
 
-Navbar.propTypes = { title: PropTypes.string.isRequired, aboutText: PropTypes.string.isRequired };
-Navbar.defaultProps = {title: 'Set title here',aboutText:'About'}
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  aboutText: PropTypes.string.isRequired,
+};
+
+Navbar.defaultProps = {
+  title: "Set title here",
+  aboutText: "About",
+};
+
